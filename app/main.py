3d0b89e930dc16a5dd81faa287fda76c4cc1ee2d
amos <752711024@qq.com>
2024-07-01 16:37:39 +0800

    fix

--- app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.v1.endpoints import orders
-
-app = FastAPI(title="BI Project API", version="1.0.0")
-
-app.include_router(orders.router, prefix="/api/v1/orders", tags=["orders"])
