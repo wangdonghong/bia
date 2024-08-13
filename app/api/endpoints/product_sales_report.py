744991786a0220cd17c131b7fecd521eedb1a8b7
amos <752711024@qq.com>
2024-08-13 19:11:17 +0800

    fix

--- app/api/endpoints/product_sales_report.py
+++ app/api/endpoints/product_sales_report.py
@@ -83,9 +83,9 @@ def query_product_sales_report(params: ProductSalesReportParams) -> Dict[str, An
     items = {product_id: {"qty": qty_data["qty"], "gmv": qty_data["gmv"]} for product_id, qty_data in data_dict.items()}
 
     result = {
-            "xAxis": item_dates,
-            "items": [items]
-        }
+        "xAxis": item_dates,
+        "items": items
+    }
 
     total = len(items)
 
